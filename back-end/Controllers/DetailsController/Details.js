72a010170566c58e46d6f9ca5eac5cd2ec0ce10f
Renato <re.fbarcellos@hotmail.com>
2020-08-02 14:58:42 -0400

    :zap: improve the performace of call the API
    ReWrite the scripts that call the api to a format that takes half of the time to get all the information that we need. Missing cover Image yet

--- back-end/Controllers/DetailsController/Details.js
+++ back-end/Controllers/DetailsController/Details.js
@@ -5,74 +5,84 @@ show to our user the game that he clicked at
 */
 
 const axios = require('axios').default;
-const Utils = require('./Utils')
+
+module.exports = {
+    async  Details(id){
+        //General Data
+        data = await axios({
+            
+            url: "https://api-v3.igdb.com/games/",
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'user-key': '92dc2928aab6bc080b79811519353eb5'
+            },
+            
+            data: `where id = ${id}; fields name, cover,first_release_date, genres.name,involved_companies.company.name,platforms.name,total_rating;`
+        })
+        .then(response => {
+            const data = response.data[0]
+            console.log(data)
+            return data
+        })
+        .catch(err => {
+            console.error(err);
+        });
+
+        //our variables of the game Object
+        const name = data.name
+        const release = data.first_release_date
+        const rating = Math.floor(data.total_rating)
+        const platforms = data.platforms
+        const involvedCompanies = data.involved_companies
+        const genres = data.genres
+
+        //Getting the genres of the game
+        genresNames = []
+        for(i = 0;i < genres.length;i ++){
+            genresNames.push(genres[i].name)
+        }
+
+        //Getting the companies that create the game
+        companiesNames = []
+        for(i = 0;i < involvedCompanies.length;i ++){
+            company = involvedCompanies[i].company.name
+            companiesNames.push(company)
+        }
+
+        //Getting the platforms where the game are
+        platformsNames = []
+        for(i = 0; i < platforms.length; i++){
+            platform = platforms[i].name
+            platformsNames.push(platform)
+        }
+
+        //Getting the publication year of the game 
+        const unixTimestamp = release
+        const milliseconds = unixTimestamp * 1000
+        const dateObject = new Date(milliseconds)
+        const year = dateObject.toLocaleString("en-US", {year: "numeric"}) 
+        dateObject.toLocaleString("en-US", {timeZoneName: "short"})
 
 
-async function Details(id){
-    //General Data
-    data = await axios({
-        
-        url: "https://api-v3.igdb.com/games/",
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'user-key': '92dc2928aab6bc080b79811519353eb5'
-        },
-        
-        data: `where id = ${id}; fields name, cover,first_release_date, genres,involved_companies,platforms,total_rating;`
-    })
-    .then(response => {
-        const data = response.data[0]
-        console.log(data)
-        return data
-    })
-    .catch(err => {
-        console.error(err);
-    });
+        console.log({
+            name,
+            year,
+            rating,
+            genresNames,
+            companiesNames,
+            platformsNames
+        })
 
-    //our variables of the game Object
-    const name = data.name
-    const release = data.first_release_date
-    const rating = Math.floor(data.total_rating)
-    const platforms = data.platforms
-    const involvedCompanies = data.involved_companies
-    const genres = data.genres
+        return({
+            name,
+            year,
+            rating,
+            genresNames,
+            companiesNames,
+            platformsNames
+        })
 
-
-    //Getting an array of platforms names↓
-    platformsNames = await Utils.getPlatforms(platforms)
-
-    //Getting an array of companies names↓
-    companies = await Utils.getCompanies(involvedCompanies)
-
-    //Getting an array of genres of the game ↓
-    genresNames = await Utils.getGenres(genres)
-
-    //Transforming the Unix state date into human date ↓
-    const unixTimestamp = release
-    const milliseconds = unixTimestamp * 1000
-    const dateObject = new Date(milliseconds)
-    const humanDate = dateObject.toLocaleString("en-US", {year: "numeric"}) 
-    dateObject.toLocaleString("en-US", {timeZoneName: "short"})
-
-    console.log({
-        name,
-        humanDate,
-        rating,
-        genresNames,
-        companies,
-        platformsNames
-    })
-
-    return({
-        name,
-        humanDate,
-        rating,
-        genresNames,
-        companies,
-        platformsNames
-    })
-
+    }
 }
-
-export default Details;
+Details(1942)
