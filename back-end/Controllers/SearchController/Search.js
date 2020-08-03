297bf16d15ce9c450e0621a7d6411465d45502d5
Renato <re.fbarcellos@hotmail.com>
2020-08-03 12:39:52 -0400

    :sparkles: Finish the Search function

--- back-end/Controllers/SearchController/Search.js
+++ back-end/Controllers/SearchController/Search.js
@@ -1,16 +1,52 @@
 /*
-get a gameName and search others with the same
-    search "Halo"; fieds name, image
+This function will recebe a incompleet game name
+and will return a list of games with the similar name
 
-
-Its return the image and the name of these others
-    return(name,image)
-
-on chose the game that you want enter into the List comand, to list the similars
-
-
-
-https://api-v3.igdb.com/search
-
-
+return(image cover & Correct name)
 */
+
+const axios = require('axios')
+
+module.exports = {
+
+
+    async Search(){
+        //General Data
+        const rightOnes  = await axios({
+        
+            url: "https://api-v3.igdb.com/games/",
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'user-key': '92dc2928aab6bc080b79811519353eb5'
+            },
+            
+            //asked data ↓
+            data: `fields name,cover.image_id; search "Zelda"; limit 30;`
+        }) 
+        .then(response => {
+            console.log(response.data)
+            return response.data  
+        })
+        .catch(err => {
+            console.error(err);
+        });
+
+        
+        rightNames = []
+        
+        // Ajusting the data (img url, gameId and gameName)
+        for(i = 0; i < rightOnes.length; i ++){
+            image = `https://images.igdb.com/igdb/image/upload/t_cover_big/${data[i].cover.image_id}.jpg`
+            gameList = [
+                data[i].id,
+                data[i].name,
+                image
+            ]
+            rightNames.push(gameList)
+        }
+
+        return(rightNames)
+    
+    }
+}
