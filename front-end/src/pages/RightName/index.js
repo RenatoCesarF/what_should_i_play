eb1f115fa0e5eda3644c451dc49c90fcb755c070
Renato <re.fbarcellos@hotmail.com>
2020-08-05 21:18:53 -0400

    :sparkles: Add new Page

--- front-end/src/pages/RightName/index.js
+++ front-end/src/pages/RightName/index.js
@@ -1,16 +1,27 @@
   
 import styles from './style.js'
-
-
-import SearchBar from '../../components/SearchBar/index'
 import React from 'react';
-import { View , Text,StatusBar} from 'react-native';
+import { View , Text, StatusBar } from 'react-native';
+
+import RightGame from '../../components/RightGame/index'
+import SearchBar from '../../components/SearchBar/index'
+
+//for each result of Search function will create one RightGame component
 
 export default function RightName() {
   return (
     
     <View style={styles.container}>
-      <h1> Right Name</h1>
+      
+      <View style={styles.header}>
+        <FontAwesome name="arrow-left" color="#F21B3F" size={27}
+            onPress={() =>  navigation.goBack()}
+        />
+        <FontAwesome name="bookmark" color="#F21B3F" size={27}/>
+      </View>
+
+      <RightGame/>
+      <h1> You mean:</h1>
       <StatusBar style="hide" />
     </View>
   );
--- front-end/src/pages/RightName/style.js
+++ front-end/src/pages/RightName/style.js
@@ -3,8 +3,18 @@ import {StyleSheet} from 'react-native'
 const styles = StyleSheet.create({
   container:{ 
       flex: 1,
-      backgroundColor: '#1F1A38',
-    }
+      backgroundColor: '#130d2d',
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: "100%", 
+    top: 0,
+    borderBottomWidth: 3,
+    borderColor: '#110e1F',
+    paddingHorizontal: "6%",
+    paddingVertical: "4%",
+  }
 
 });
 
